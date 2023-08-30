0a3121c26bd4786743cf515a428e108b1c69f453
Saeed1112 <nullee.sa@gmail.com>
2023-08-30 18:36:06 +0330

    .

--- app/components/ui/sections/AppHero.tsx
+++ app/components/ui/sections/AppHero.tsx
@@ -5,13 +5,10 @@ import { Scene } from "react-scrollmagic";
 import { Ndot55 } from "@/font/ndot55";
 
 function ImageCover() {
+  const from = { scale: 1 };
+  const to = { scale: 4, opacity: 0, x: "-45%" };
   return (
-    <Tween
-      position={0}
-      from={{ scale: 1 }}
-      to={{ scale: 4, opacity: 0, x: "-45%" }}
-      duration={900}
-    >
+    <Tween from={from} to={to} position={1}>
       <Image
         className="absolute left-0 top-0 z-10 h-screen w-full object-cover object-center"
         src="/images/phone-cover.webp"
@@ -24,13 +21,10 @@ function ImageCover() {
 }
 
 function PhoneName() {
+  const from = { scale: 1 };
+  const to = { opacity: 0, x: "-15%" };
   return (
-    <Tween
-      position={0}
-      from={{ scale: 1 }}
-      to={{ opacity: 0, x: "-15%" }}
-      duration={900}
-    >
+    <Tween from={from} to={to} position={1}>
       <div className="absolute z-20 flex min-h-screen w-full items-center">
         <div className={`${Ndot55.className} mr-auto w-1/2`}>
           <h2 className="mt-6 pr-16 text-right text-3xl text-[10rem] uppercase text-black">
@@ -46,14 +40,45 @@ function ThePhone() {
   const from = { scale: 3.9, y: "56.5%", rotation: 90, x: "-370%" };
   const to = { scale: 1, y: 0, rotation: 0, x: 0 };
   return (
-    <Tween from={from} to={to} duration={1000} position={2}>
-      <Image
-        className="w-64"
-        src="/images/phone.webp"
-        alt="Nothing phone 2"
-        height="960"
-        width="1980"
-      />
+    <div className="flex w-1/2 items-center justify-center">
+      <Tween from={from} to={to}>
+        <Image
+          className="w-64"
+          src="/images/phone.webp"
+          alt="Nothing phone 2"
+          height="960"
+          width="1980"
+        />
+      </Tween>
+    </div>
+  );
+}
+
+function CameraCover() {
+  const from = { opacity: 1 };
+  const to = { opacity: 0 };
+  return (
+    <Tween from={from} to={to}>
+      <div className="absolute left-0 top-0 z-10 min-h-screen w-full bg-neutral-950" />
+    </Tween>
+  );
+}
+
+function PhoneDetailCard() {
+  const from = { opacity: 0 };
+  const to = { opacity: 1 };
+  return (
+    <Tween from={from} to={to}>
+      <div className="flex w-1/2 flex-col">
+        <h2 className="text-heading font-light text-gray-50">ناتینگ فون 2</h2>
+        <ul className="text-body text-neutral-500">
+          <li>رم: 12گیگابایت</li>
+          <li>پردازنده: Snapdragon 8+ Gen 1</li>
+          <li>صفحه نمایش: LTPO OLED</li>
+          <li>باطری: 4700 میلی آمپر ساعت</li>
+          <li>دوربین: 50/50مگاپیکسل</li>
+        </ul>
+      </div>
     </Tween>
   );
 }
@@ -69,22 +94,10 @@ const AppHero = () => {
       {(progress: number) => (
         <div className="flex min-h-screen w-full items-center justify-center overflow-hidden">
           <Timeline duration={3000} totalProgress={progress}>
+            <CameraCover />
             <div className="container mx-auto flex min-h-screen items-center gap-10">
-              <div className="flex w-1/2 items-center justify-center">
-                <ThePhone />
-              </div>
-              <div className="flex w-1/2 flex-col">
-                <h2 className="text-heading font-light text-gray-50">
-                  ناتینگ فون 2
-                </h2>
-                <ul className="text-body text-neutral-700">
-                  <li>رم: 12گیگابایت</li>
-                  <li>پردازنده: Snapdragon 8+ Gen 1</li>
-                  <li>صفحه نمایش: LTPO OLED</li>
-                  <li>باطری: 4700 میلی آمپر ساعت</li>
-                  <li>دوربین: 50/50مگاپیکسل</li>
-                </ul>
-              </div>
+              <ThePhone />
+              <PhoneDetailCard />
             </div>
             <ImageCover />
             <PhoneName />
